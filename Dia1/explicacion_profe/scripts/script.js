259df84a595e7023d297f5baf053f2223ce8a661
Alejandro <yasdmi048@gmail.com>
2025-07-18 12:25:13 -0500

    feat :sparkles: finish day1 explanation teacher

--- Dia1/explicacion_profe/scripts/script.js
+++ Dia1/explicacion_profe/scripts/script.js
@@ -4,4 +4,34 @@
 
 //¡¡¡ primer comando !!!
 console.log ("hola mundo");
+variable1= 3.14;
+console.log (variable1);
+console.log (typeof variable1);
+// concentracion 
+palabra1= "campus";
+palabra2= "lands";
+palabra3= palabra1 + palabra2;
+console.log (palabra3);
+
+nombrepersona= "luis angel";
+plataactual= 5000,
+console.log ("la persona",nombrepersona,"tiene" ,plataactual, "pesos colombianos");
+// boleanos
+boleanos= true;
+// condicionales
+if (boleanos == true){
+    console.log(" es verdadero");}
+    
+else {("no es verdadero ");} 
+// ciclos 
+for (i=0;i<5;i=i+1){
+    console.log(i, "wenas");
+}
+holi=true;
+while (holi==true){
+    console.log("hola chamo");
+    holi=false ;
+}
+
+
 // desarrollado por Alejandro Andres Sanchez carrillo
