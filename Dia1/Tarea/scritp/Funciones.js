53ee3afd52d39f0f43d9a447a45f12d0111b824b
unknown <yasdmi048@gmail.com>
2025-07-19 20:19:36 -0500

    feat :construction: in construction path funciones

--- Dia1/Tarea/scritp/Funciones.js
+++ Dia1/Tarea/scritp/Funciones.js
@@ -1,48 +1,90 @@
-
-//creo una funcion para el menu del simluadorde gasto 
-function simuladorGasto (){
-    boleano=True;
-    while (boleano){
+//creo una funcion para el menu del simulador de gasto 
+function simuladorGasto() {
+    let boleano = true;
+    let gastos1 = [];
+    while (boleano) {
         //simulador de gasto 
-        console.log ("=============================================");
-        console.log ("        Simulador de Gasto Diario            ");
-        console.log ("=============================================");
-        console.log ("          Seleccione una opción:             ");
-        console.log ("         1. Registrar/eliminar/modificar gasto ");
-        console.log ("         2. Listar gastos                  ");
-        console.log ("         3. Calcular total de gastos");
-        console.log ("         4. Generar reporte de gastos");
-        console.log ("         5. Salir");
-        console.log ("=============================================");
-         let opcion=prompt("eliga una opción numerica :");
-        if (opcion==1 ){
-            nuevoGasto();
-        }
-            
-        else if (opcion==2) {
-            lista_gasto();
-        }
-            
-        else if (opcion==3){
-            calcularGasto();
-        }
-            
-        else if (opcion==4){
-            generarReporte();
+        console.log("=============================================");
+        console.log("        Simulador de Gasto Diario            ");
+        console.log("=============================================");
+        console.log("          Seleccione una opción:             ");
+        console.log("         1. Registrar/eliminar/modificar gasto ");
+        console.log("         2. Listar gastos                  ");
+        console.log("         3. Calcular total de gastos");
+        console.log("         4. Generar reporte de gastos");
+        console.log("         5. Salir");
+        console.log("=============================================");
+        let opcion = prompt("eliga una opción numerica :");
 
+        if (opcion == 1) {
+            nuevoGasto(gastos1);
+        } else if (opcion == 2) {
+            lista_gasto(gastos1);
+        } else if (opcion == 3) {
+            calcularGasto(gastos1);
+        } else if (opcion == 4) {
+            generarReporte(gastos1);
+        } else if (opcion == 5) {
+            boleano = false;
+            console.log("ha salido del programa ");
+        } else {
+            console.log("opcion no valida");
         }
-        else if (opcion==5){
-            boleano=False;
-            console.log ("ha salido del programa ");
-        }
-        else (console.log("opcion no valida"))
-            
-
     }
-       
-
 }
-function generarReporte(){
+
+function nuevoGasto(gastos1) {
+    console.log("menu de gastos");
+    console.log("1.agregar un gasto ");
+    console.log("2.modificar gasto ");
+    console.log("3.eliminar gasto ");
+    let opc = parseInt(prompt("ingrese opcion numerica:"));
+    let descripcion = "";
+
+    if (opc == 1) {
+        let monto = parseFloat(prompt("digite su gasto:"));
+        let categoria = prompt("digita la categoria:");
+        let fecha = prompt("digite la fecha dd/mm/yyyy:");
+        let opinion = parseInt(prompt("quieres agregar una descripcion?:1:si  2:no:"));
+        if (opinion == 1) {
+            descripcion = prompt("escribe la descripcion");
+        } else {
+            descripcion = "";
+            console.log("no se agrego ninguna descripcion");
+        }
+        let gasto = {
+            monto: monto,
+            categoria: categoria,
+            fecha: fecha,
+            descripcion: descripcion
+        };
+        gastos1.push(gasto);
+        console.log("se añadio el gasto");
+    } else if (opc == 2) {
+        let ultimo = gastos1.length - 1;
+        let monto = parseFloat(prompt("Nuevo monto:"));
+        let categoria = prompt("Nueva categoría:");
+        let fecha = prompt("Nueva fecha (dd/mm/yyyy):");
+        let descripcion = prompt("Nueva descripción:");
+        gastos1[ultimo].monto = monto;
+        gastos1[ultimo].categoria = categoria;
+        gastos1[ultimo].fecha = fecha;
+        gastos1[ultimo].descripcion = descripcion;
+    } else if (opc == 3) {
+        console.log(gastos1);
+        let confirmarcion = prompt("¿Estás seguro que quieres eliminar el último gasto? (s/n:)");
+        if (confirmarcion == "s") {
+            gastos1.pop();
+            console.log("gasto eliminado ");
+        } else {
+            console.log("no se elimino ");
+            prompt("enter para continuar");
+        }
+    }
+    return gastos1;
+}
+
+function generarReporte(gastos1) {
     console.log("=============================================");
     console.log("           Generar Reporte de Gastos         ");
     console.log("=============================================");
@@ -51,38 +93,97 @@ function generarReporte(){
     console.log("      3. Mensual");
     console.log("      4. Regresar al menú principal");
     console.log("=============================================");
-    let opc =prompt ("digite opcion numerica:");
-    opc=parseInt(opc);
-    total=0;
-    diccionarioCategoria={};
+    let opc = parseInt(prompt("digite opcion numerica:"));
+
+    if (opc == 1) {
+        console.log(gastos1);
+        console.log("formato esperado : dd/mm/yyyy");
+        let fechabusqueda = prompt("digite la fecha exacta para calcular total de gasto :");
+        let x = true;
+        while (x == true) {
+            console.log("=============================================");
+            console.log("             ¿Que desea hacer?");
+            console.log("=============================================");
+            console.log("               1.ver reporte");
+            console.log("               2.guardar reporte");
+            console.log("               3.atras");
+            console.log("=============================================");
+            let opcion = prompt(" digite opcion numerica:");
+            opcion = parseInt(opcion);
+            if (opcion == 1) {
+                for (let i = 0; i < gastos1.length; i += 1) {
+                    if (gastos1[i].fecha == fechabusqueda) {
+                        console.log(`${gastos1[i].categoria} - $${gastos1[i].monto} - ${gastos1[i].descripcion}`);
+                    }
+                }
+            } 
+            else if (opcion == 2) {
+                console.log("se guardó correctamente el reporte");
+            } 
+            else if (opcion == 3) {
+                x = false;
+            }
+        }
+    }
+    if (opc == 2) {
+        console.log(gastos1);
+        console.log("formato esperado : dd/mm/yyyy");
+        let fechabusqueda = prompt("digite la fecha exacta para calcular total de gasto :");
+        let x = true;
+        while (x == true) {
+            console.log("=============================================");
+            console.log("             ¿Que desea hacer?");
+            console.log("=============================================");
+            console.log("               1.ver reporte");
+            console.log("               2.guardar reporte");
+            console.log("               3.atras");
+            console.log("=============================================");
+            let opcion = prompt(" digite opcion numerica:");
+            opcion = parseInt(opcion);
+            if (opcion == 1) {
+                for (let i = 0; i < gastos1.length; i += 1) {
+                    if (gastos1[i].fecha == fechabusqueda) {
+                        console.log(`${gastos1[i].categoria} - $${gastos1[i].monto} - ${gastos1[i].descripcion}`);
+                    }
+                }
+            } else if (opcion == 2) {
+                console.log("se guardó correctamente el reporte");
+            } else if (opcion == 3) {
+                x = false;
+            }
+        }
+    }
+
+    if (opc == 3) {
+        console.log(gastos1);
+        console.log("formato esperado : dd/mm/yyyy");
+        let fechabusqueda = prompt("digite la fecha exacta para calcular total de gasto :");
+        let x = true;
+        while (x == true) {
+            console.log("=============================================");
+            console.log("             ¿Que desea hacer?");
+            console.log("=============================================");
+            console.log("               1.ver reporte");
+            console.log("               2.guardar reporte");
+            console.log("               3.atras");
+            console.log("=============================================");
+            let opcion = prompt(" digite opcion numerica:");
+            opcion = parseInt(opcion);
+            if (opcion == 1) {
+                for (let i = 0; i < gastos1.length; i += 1) {
+                    if (gastos1[i].fecha == fechabusqueda) {
+                        console.log(`${gastos1[i].categoria} - $${gastos1[i].monto} - ${gastos1[i].descripcion}`);
+                    }
+                }
+            } else if (opcion == 2) {
+                console.log("se guardó correctamente el reporte");
+            } else if (opcion == 3) {
+                x = false;
+            }
+        }
+    }
+
+    if (opc == 4) {
+        console.log("menu principal");
+    }
 }
-    
-    
-   
-    
-    if (opc ==1){ 
-        console.log ("formato esperado : dd/mm/yyyy");
-        let fechaInicio =prompt("digite la fecha exacta para calcular total de gasto :");
-        fechaInicio=parseInt(fechaInicio);
-       
-        for (let gasto of usuarioActual.gastos){
-            console.log ("=============================================");
-            console.log  ("             ¿Que desea hacer?");
-            console.log("=============================================");
-            console.log ("               1.ver reporte");
-            console.log ("               2.guardar reporte");
-            console.log("=============================================");
-            let opcion=prompt(" digite opcion numerica:");
-            opcion=parseInt(opcion);
-        if (opcion==1){
-            reporte=verYguardar_reporte(fechaInicio,diccionarioCategoria,total);
-        }
-            
-            console.log("se guardó correctamente el reporte: {reporte}")};
-    
-    
-        }
-        
-               
-                        
-       
