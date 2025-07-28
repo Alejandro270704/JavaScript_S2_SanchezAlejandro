d5c6ebb1b3f12088b140ae3ba6f07e5614b091b0
unknown <yasdmi048@gmail.com>
2025-07-28 17:40:17 -0500

    feat :white_check_mark: finished job

--- Dia6/main.js
+++ Dia6/main.js
@@ -1,7 +1,10 @@
 function personaje(nombre,done){
     fetch("https://rickandmortyapi.com/api/character/?name=" + nombre)
     .then(response =>response.json())
-    .then(data=> {done (data)});
+    .then(data=> {done (data)})
+    .catch(error=>{
+        alert("no existe el perosnaje")
+    })
     
 }
 function menu (){
@@ -10,12 +13,32 @@ function menu (){
         escriba el nombre del personaje a consultar
         ============================================ `).toLowerCase()
         personaje(nombre ,function (data){
-            if (data.results,data.length > 0){
-                alert(`
-                    ============================================
-                    `)
+            if ( data.results && data.results.length > 0){
+                let mensaje ="";
+                data.results.forEach(personaje => {
+                    mensaje +=(`
+                =========================================
+                id : ${personaje.id}
+                nombre: ${personaje.name}
+                status: ${personaje.status}
+                gender: ${personaje.gender}
+                origin: ${personaje.origin.name}
+                location: ${personaje.location.name}
+                =========================================`)
+                    
+                }
+            )
+                
+                
+            alert(mensaje)       
             }
+            else {
+                alert("no hay personajes ")}
+            menu ()
         }
+        
     )
     
+    
 }
+menu()
