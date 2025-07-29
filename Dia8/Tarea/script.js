5329a6dc46b77c9a9ddfb3d95f8b9732a7434fe3
unknown <yasdmi048@gmail.com>
2025-07-29 16:50:57 -0500

    feat :art: added planet in person

--- Dia8/Tarea/script.js
+++ Dia8/Tarea/script.js
@@ -1,15 +1,37 @@
 function buscarpersonaje (){
     const id=(prompt(" ingresa el id del personaje "));
     const xml=  new XMLHttpRequest();
-    const url= `https://swapi.py4e.com/api/people/?:id=${id}`;
+    const url= `https://swapi.py4e.com/api/people/${id}/`;
     xml.open("GET" ,url,true);
     xml.onreadystatechange=function(){ 
         if (xml.readyState === 4 && xml.status === 200){
             try {
                 const datos=JSON.parse(xml.responseText);
-                console.log(" personaje:\nname:"+datos["results"][id]["name"]+"\nheight:"+datos["results"][id]["height"]+"\nmass:"+datos["results"][id]["mass"]);
-                
-            }
+
+                const urlplaneta=datos.homeworld;
+                const xmlplaneta= new XMLHttpRequest();
+                xmlplaneta.open("GET",urlplaneta,true);
+                xmlplaneta.onreadystatechange=function(){
+                    if (xmlplaneta.readyState === 4 && xmlplaneta.status === 200) {
+                        const planetadatos = JSON.parse(xmlplaneta.responseText);
+                        console.log(
+                `Personaje:
+name: ${datos.name}
+height: ${datos.height}
+mass: ${datos.mass}
+hair_color: ${datos.hair_color}
+skin_color: ${datos.skin_color}
+eye_color: ${datos.eye_color}
+birth_year: ${datos.birth_year}
+gender: ${datos.gender}
+planeta:${planetadatos.name}`)
+                        
+                }
+            
+                }
+                xmlplaneta.send();
+                }
+            
             
             catch(err){
                 console.log(err.message);
