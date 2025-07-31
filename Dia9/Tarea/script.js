94cbec9ef2def751535a06e771d75db101237a53
unknown <yasdmi048@gmail.com>
2025-07-30 21:21:02 -0500

    feat :sparkles: function buton

--- Dia9/Tarea/script.js
+++ Dia9/Tarea/script.js
@@ -1,6 +1,6 @@
-
-function buscarpersonaje (){
-    const personaje =document.getElementById("buscanombre").value.toLowerCase() ;
+let idpokemon = 0;
+function buscarpersonaje (idnuevo){
+    const personaje = idnuevo ||document.getElementById("buscanombre").value.toLowerCase() ;
     const xml=  new XMLHttpRequest();
     const url= `https://pokeapi.co/api/v2/pokemon/${personaje}/`;
     xml.open("GET" ,url,true);
@@ -8,12 +8,13 @@ function buscarpersonaje (){
         if (xml.readyState === 4 && xml.status === 200){
             try {
                 const datos=JSON.parse(xml.responseText);
-                const imagen=document.getElementById("imagen");
-                imagen.src =datos.sprites.front_default; 
+                const imagen1=document.getElementById("imagen");
+                imagen1.innerHTML= ` <img src=${datos.sprites.front_default}>`
                 const id = document.getElementById("numero");
                 id.innerHTML=`${datos.id}`;
                 const name =document.getElementById("nombrepokemon");
                 name.innerHTML = `${datos.name}`
+                idpokemon= datos.id
             }
             catch(err){
                 console.log(err.message);
@@ -28,3 +29,15 @@ function buscarpersonaje (){
             buscarpersonaje();
         }
     });
+    const botonprev= document.getElementById("atras");
+    botonprev.addEventListener ("click",  ()=>{
+        if (idpokemon> 1){
+            buscarpersonaje(idpokemon-1);
+        }
+    })
+    const botonadelante= document.getElementById("adelante");
+    botonadelante.addEventListener ("click",  ()=>{
+
+            buscarpersonaje(idpokemon+1);
+        }
+    )
