aaa4ff3eb213d30ec028a2187c6dd805937f0c68
unknown <yasdmi048@gmail.com>
2025-08-01 17:11:11 -0500

    feat :construction: in construction menu

---
+++ Dia10/script.js
@@ -0,0 +1,15 @@
+
+function desaparecer() {
+    document.querySelector('.titulo').classList.add('desaparecerelemento');
+    document.querySelector('.texto1').classList.add('desaparecerelemento');
+    document.querySelector('.entrar').classList.add('desaparecerelemento');
+    
+    setTimeout(()=>{
+        const menu =document.querySelector(`.menu`);
+        menu.style.display=`flex`;
+        menu.classList.add(`aparecer`)
+    },600)
+    
+
+  }
+  
