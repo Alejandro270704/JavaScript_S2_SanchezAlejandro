1d5e9fe1c4eb3ae2b170e9d0885d57d480f2ec73
unknown <yasdmi048@gmail.com>
2025-08-02 14:51:21 -0500

    feat :sparkles: added music

--- Dia10/script.js
+++ Dia10/script.js
@@ -1,5 +1,6 @@
-
 function desaparecer() {
+    const musica= document.getElementById(`musicafondo`);
+    musica.play();
     document.querySelector('.titulo').classList.add('desaparecerelemento');
     document.querySelector('.texto1').classList.add('desaparecerelemento');
     document.querySelector('.entrar').classList.add('desaparecerelemento');
@@ -9,6 +10,18 @@ function desaparecer() {
         menu.style.display=`flex`;
         menu.classList.add(`aparecer`)
     },600)
-    
 
   }
+function mostrarreglas() {
+    document.querySelector('.menu').style.display='none';
+    
+    const contenido = document.querySelector('.contenido');
+    contenido.style.display = 'none';
+    contenido.style.visibility = 'hidden';
+    contenido.style.opacity = 0;
+    contenido.style.backgroundImage = 'none';
+
+    const instrucciones = document.querySelector('.instrucciones-1');
+    instrucciones.style.display='block';
+    instrucciones.classList.add('aparecer2');
+}
